79bf124191f1a5f008fad3219386ed2106f26e0a
legendbears <erickim1006@gmail.com>
2022-05-12 02:17:07 +0900

    first commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>Eric Kim</h1>
+      <h2>
+        Jayang middle school-Paichai high school-Hanyang University
+      </h2>
+	  <h3>
+		participants in the HYU:League gomoku competition
+		</h3>
+    </>
+  );
+}
